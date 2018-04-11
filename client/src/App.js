f170be5665fa11c886f19165ab3c189d39210f65
Josh Sample <joshuawsample@gmail.com>
2018-04-11 17:38:12 -0400

    installed necessary modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/guidestones.jpg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import axios from 'axios'
+import { injectGlobal } from 'styled-components'
+
+import LandingView from './components/universalcomponents/LandingView'
+import IndexView from './components/universalcomponents/IndexView'
 
 class App extends Component {
   render() {
@@ -8,10 +14,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to The Thunderdome</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          bitches
+          
         </p>
       </div>
     );
