f33cad05ead90f7447fb54a59b11e842e02fca1d
Josh Sample <joshuawsample@gmail.com>
2018-04-11 16:04:34 -0400

    Created folders and components

---
+++ client/src/components/usercomponents/SignUpUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignUpUser extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default SignUpUser;
