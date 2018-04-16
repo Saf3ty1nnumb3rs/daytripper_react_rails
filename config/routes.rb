402a8be10bf8470efa9b31db03b7054a3322547e
Josh Sample <joshuawsample@gmail.com>
2018-04-16 10:34:48 -0400

    Setting up http party

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  patch '/locate', to: 'api/destinations#locate'
+
+  post '/search', to: 'api/destinations#search'
+
+  post '/nav', to: 'api/destinations#show'
+  
+  
+  
+  
+  
   namespace :api do
     resources :users
     resources :destinations do
